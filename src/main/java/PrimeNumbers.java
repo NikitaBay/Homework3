ac819a40fd3c7d22e76b3fac17f6f1eef6010be0
NikitaBay <n.bayushev2017@yandex.ru>
2023-11-06 20:06:54 +0400

    fix cases: negative, 0, 1 numbers

--- src/main/java/PrimeNumbers.java
+++ src/main/java/PrimeNumbers.java
@@ -2,33 +2,38 @@ import java.util.Scanner;
 
 public class PrimeNumbers {
     public static void main(String[] args) {
-        long number;
-        boolean isPrime = true;
-        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            long number;
+            boolean isPrime = true;
+            Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Введите число: ");
+            System.out.print("Введите число: ");
 
-        if (scanner.hasNextLong()) {
-            number = scanner.nextLong();
+            if (scanner.hasNextLong()) {
+                number = scanner.nextLong();
 
-            if (number <= 1) {
-                isPrime = false;
-            } else {
-                for (long i = 2; i * i <= number; i++) {
-                    if (number % i == 0) {
-                        isPrime = false;
-                        break;
+                if (number <= 1) {
+                    isPrime = false;
+                } else {
+                    for (long i = 2; i * i <= number; i++) {
+                        if (number % i == 0) {
+                            isPrime = false;
+                            break;
+                        }
                     }
                 }
-            }
-            if (isPrime) {
-                System.out.println(number + " - Простое число");
-
+                if (number < 0) {
+                    System.out.println(number + " - Отрицательное число не является натуральным");
+                } else if (number == 1 || number == 0) {
+                    System.out.println(number + " - Ни простое, ни составное");
+                } else if (isPrime) {
+                    System.out.println(number + " - Простое число");
+                } else {
+                    System.out.println(number + " - Составное число");
+                }
             } else {
-                System.out.println(number + " - Составное число");
+                System.out.println("Это не число брат");
             }
-        } else {
-            System.out.println("Это не число брат");
         }
     }
 }
